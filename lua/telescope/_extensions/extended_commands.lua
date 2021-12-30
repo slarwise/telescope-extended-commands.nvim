75e8227d2b97aec7ebe5e6ec13e1cd5ccc6e68de
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-12-30 20:51:56 +0100

    First working draft

---
+++ lua/telescope/_extensions/extended_commands.lua
@@ -0,0 +1,59 @@
+local has_telescope, telescope = pcall(require, "telescope")
+if not has_telescope then
+    error "This plugins requires nvim-telescope/telescope.nvim"
+end
+
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local make_entry = require "telescope.make_entry"
+
+local extended_commands = function(opts)
+    opts = opts or {}
+
+    local command_names = vim.fn.getcompletion("", "command")
+
+    local user_commands = vim.api.nvim_get_commands {}
+    for name, values in pairs(user_commands) do
+        -- Including customlist took a long time
+        -- if vim.tbl_contains({ "custom", "customlist" }, values.complete) then
+        if vim.tbl_contains({ "custom" }, values.complete) then
+            local custom_completions = vim.fn.getcompletion(name .. " ", "cmdline")
+            for _, sub_command in ipairs(custom_completions) do
+                table.insert(command_names, name .. " " .. sub_command)
+            end
+        end
+    end
+
+    pickers.new(opts, {
+        prompt_title = "Commands",
+        finder = finders.new_table {
+            results = command_names,
+        },
+        entry_maker = opts.entry_maker or make_entry.gen_from_commands(opts),
+        previewer = conf.grep_previewer(opts),
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr)
+            actions.select_default:replace(function()
+                local selection = action_state.get_selected_entry()
+                if selection == nil then
+                    print "[telescope] Nothing currently selected"
+                    return
+                end
+
+                actions.close(prompt_bufnr)
+                local cmd = selection.value
+
+                vim.cmd(cmd)
+            end)
+
+            return true
+        end,
+    }):find()
+end
+
+return telescope.register_extension {
+    exports = { extended_commands = extended_commands },
+}
